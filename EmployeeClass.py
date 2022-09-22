1f089800727f5bd77acf5f3d6b4434d3fc65e235
Danny <dannythekid1292@gmail.com>
2022-09-22 13:44:43 -0500

    updated with objects

--- EmployeeClass.py
+++ EmployeeClass.py
@@ -1,22 +1,22 @@
 class Employee:
-    def __init__(self, name, ID, number, department, title):
+    def __init__(self, name, IDnumber, department, title, monthlySalary):
         self.__name = name
-        self.__ID = ID
-        self.__number = number
+        self.__IDnumber = IDnumber
         self.__department = department
         self.__title = title
+        self.__monthlySalary = monthlySalary
 
     def get_name(self):
         return self.__name
 
-    def get_ID(self):
-        return self.__ID
-
-    def get_number(self):
-        return self.__number
+    def get_ID_number(self):
+        return self.__IDnumber
 
     def get_department(self):
         return self.__department
     
     def get_title(self):
         return self.__title
+
+    def get_monthly_salary(self):
+        return self.__monthlySalary
--- NetPay.py
+++ NetPay.py
@@ -1,2 +1,28 @@
 import EmployeeClass as emp
 import PayrollDeductionsClass as pd
+
+
+jimmys_record = emp.Employee("Jimmy Smith", 58475, "IT", "Developer", 6800.00)
+
+transaction_1 = pd.PayrollDeductions("food court", "8/14/2022", 22.50, 39119)
+transaction_2 = pd.PayrollDeductions("gift contribution", "8/12/2022", 25.00, 58475)
+transaction_3 = pd.PayrollDeductions("food court", "8/17/2022", 15.25, 21547)
+transaction_4 = pd.PayrollDeductions("vending machine", "8/22/2022", 3.00, 58475)
+transaction_5 = pd.PayrollDeductions("vending machine", "8/5/2022", 2.75, 58475)
+
+transactions = [transaction_1, transaction_2, transaction_3, transaction_4, transaction_5]
+
+jimmys_bill_total = 0
+
+for transaction in transactions:
+    if transaction.get_employee_ID() == jimmys_record.get_ID_number():
+        jimmys_bill_total += transaction.get_charge()
+
+
+print(
+    f'''** Employee Pay **
+Name: {jimmys_record.get_name()}
+ID Number: {jimmys_record.get_ID_number()}
+Department: {jimmys_record.get_department()}
+Gross Pay: ${jimmys_record.get_monthly_salary()}
+Net Pay ${float(jimmys_record.get_monthly_salary() - jimmys_bill_total)}''')
